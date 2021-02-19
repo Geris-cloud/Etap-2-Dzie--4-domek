076c18f8e8499772cbf26869ec9a4787836e6fd8
Dariusz Ochotny <dariuszochotny9@gmail.com>
2021-02-19 18:37:09 +0100

    Update build.js

--- build.js
+++ build.js
@@ -2,22 +2,10 @@ const elements = [...document.querySelectorAll('div')];
 const btn = document.querySelector('span');
 
 btn.addEventListener('click', () => {
-    if (elements[0].haveClass = ('one')) {
-        elements[0].classList.toggle('buildone')
-    }
-    if (elements[1].haveClass = ('two')) {
-        elements[1].classList.toggle('buildtwo')
-    }
-    if (elements[2].haveClass = ('three')) {
-        elements[2].classList.toggle('buildthree')
-    }
-    if (elements[3].haveClass = ('four')) {
-        elements[3].classList.toggle('buildfour')
-    }
-    if (elements[4].haveClass = ('five')) {
-        elements[4].classList.toggle('buildfive')
-    }
-    if (elements[5].haveClass = ('six')) {
-        elements[5].classList.toggle('buildsix')
-    }
+    elements[0].classList.toggle('buildone');
+    elements[1].classList.toggle('buildtwo');
+    elements[2].classList.toggle('buildthree');
+    elements[3].classList.toggle('buildfour');
+    elements[4].classList.toggle('buildfive');
+    elements[5].classList.toggle('buildsix');
 })
